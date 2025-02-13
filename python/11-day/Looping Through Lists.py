99f38d572b57ab3dc9a60d20f342cce693f6f865
Keshav HL <keshavhlkesh@gmail.com>
2025-02-13 13:51:43 +0530

    Add files via upload

---
+++ python/11-day/Looping Through Lists.py
@@ -0,0 +1,23 @@
+#Sum of all numbers in a list/
+numbers = [10, 20, 30, 40, 50]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Total sum:", total)
+
+#Doubling each number in a list/
+numbers = [1, 2, 3, 4, 5]
+doubled = []
+
+for num in numbers:
+    doubled.append(num * 2)
+
+print("Doubled List:", doubled)
+
+#Printing Kannada food items/
+foods = ["Dosa", "Idli", "Vada", "Bisibelebath"]
+
+for food in foods:
+    print(f"I like {food}")
