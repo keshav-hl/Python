f736933b31c46dbe90545a5056fe48c009849f76
Keshav HL <keshavhlkesh@gmail.com>
2025-02-17 20:17:39 +0530

    Add files via upload

---
+++ python/home works/day-5.py
@@ -0,0 +1,18 @@
+#first home work problem/
+l = ["kesahv", "pramod","dilip","shiva"]
+
+l.append("abhi")
+print(l)
+l.insert(2,"karthic")
+print(l)
+l.pop(2)
+print(l)
+print(l.index("pramod"))
+print(" ")
+
+#second home work problem/
+num = [12,23,32,43,52,13,43,26]
+
+print(sorted(num))
+num.reverse()
+print(num)
