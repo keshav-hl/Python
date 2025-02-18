a0788eafbd51aec5382f3c225e32208dde545053
Keshav HL <keshavhlkesh@gmail.com>
2025-02-18 15:23:08 +0530

    Add files via upload

---
+++ python/home works/day-8.py
@@ -0,0 +1,30 @@
+#first home work problem/
+age = int(input("Enter the age: "))
+if age<=5:
+    print("They get free ticket..")
+elif age>=60:
+    print("They are eligible for senior citizen means free ticket..")
+else:
+    print("They should pay full ticket price..")
+
+#second home work problem/
+time = int(input("Enter the time: "))
+
+if time == 8:
+    print("It is break fast time..")
+elif time == 1:
+    print("It is a lunch time..")
+elif time == 8:
+    print("It is a dinner time..")
+else:
+    print("This is not eating time.")
+
+#third home work problem/
+age = int(input("Enter the age: "))
+
+if age <= 18:
+    print("you get student member ship id..")
+elif age>60:
+    print("you get sciner citizan ship id..")
+else:
+    print("you should get full fee or regular membership")
