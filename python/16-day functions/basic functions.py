c038a745dceefb780705c23169b310e8db6e4e2a
Keshav HL <keshavhlkesh@gmail.com>
2025-02-20 07:51:23 +0530

    Add files via upload

---
+++ python/16-day functions/basic functions.py
@@ -0,0 +1,9 @@
+#Basic function to greet a user/
+def greed():
+    print("Welcome!, To a python cource.")
+greed()
+
+#Function with a parameter/
+def student(name):
+    print(f"hello {name}. Take care.")
+student("keshav")
