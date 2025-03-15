26d9b7a78cce01a1e285a9b4be1ee4657c3275d8
Keshav HL <keshavhlkesh@gmail.com>
2025-03-15 15:36:02 +0530

    Add files via upload

---
+++ python/19-day constructers and self keyword/Optional Parameters.py
@@ -0,0 +1,17 @@
+#Optional Parameters in Constructors/
+class Book:
+    def __init__(self, title, author="Unknown"):
+        self.title = title
+        self.author = author
+
+    def show_book(self):
+        print(f"Title: {self.title}, Author: {self.author}")
+
+book1 = Book("Python Programming")
+book2 = Book("Machine Learning", "Andrew Ng")
+
+book1.show_book()
+print(book1.title)
+book2.show_book()
+print(book2.title)
+print(book2.author)
