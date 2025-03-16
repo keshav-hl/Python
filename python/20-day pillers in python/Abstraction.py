19a0771a3919722870dbe0e507f10cbcafa29c88
Keshav HL <keshavhlkesh@gmail.com>
2025-03-16 09:13:08 +0530

    Add files via upload

---
+++ python/20-day pillers in python/Abstraction.py
@@ -0,0 +1,13 @@
+#Real abstraction code/
+class car:
+    def car_start(car):
+        print("Car started..")
+    def car_stop(car):
+        print("Car stoped..")
+    def car_go_fast(car):
+        print("Press the Accelerator..")
+car1 = car()
+car1.car_go_fast()
+car1.car_stop()
+car1.car_start()
+print(" ")
