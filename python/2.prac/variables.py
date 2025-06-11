90a04f1e3d9bb24bfa4b470e7e318c0b78917490
Keshav HL <keshavhlkesh@gmail.com>
2025-06-11 20:37:29 +0530

    Add files via upload

---
+++ python/2.prac/variables.py
@@ -0,0 +1,30 @@
+#variables/
+a = 10
+b = 20
+
+print(a+b)
+
+#second variable assignement/
+a, b, c = 10, 20, 30
+print(a+b+c)
+
+#third variable assignement/
+a = b = c = 10
+print(a+b+c)
+
+#type check/
+a = "keshav"
+b = 10
+c = 20.1
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+#type conversion/
+a = "10"
+b = 20
+
+print(int(a)+b)
