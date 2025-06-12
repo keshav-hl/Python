1828e3e7a7773a43601407ce80fa5e1cdb86cbe1
Keshav HL <keshavhlkesh@gmail.com>
2025-06-12 09:41:03 +0530

    Add files via upload

---
+++ python/3.prac/indexing.py
@@ -0,0 +1,17 @@
+#Accessing string letters/
+name = "my name is keshav"
+print(name[1])
+print(name[3:])
+print(name[::-1])
+print(name[3:8])
+print(name[-6:-1])
+print(name[2:15:2])
+print(len(name))
+
+#Excape sequence/
+bio = "my name is keshav. \niam from hullikate."
+print(bio)
+bio = "my name is keshav. \tiam from hullikate."
+print(bio)
+bio = "my name is keshav. \\iam from hullikate."
+print(bio)
