a157df3c117714cbe9319386fb402589455cd4d7
Keshav HL <keshavhlkesh@gmail.com>
2025-06-12 09:08:13 +0530

    Add files via upload

---
+++ python/3.prac/string manuplutation.py
@@ -0,0 +1,19 @@
+#Concatenation/
+boy_name = "ram"
+girl_name = "sita"
+
+print(girl_name + " loves " + girl_name)
+
+#Example  - 2/
+first_name = "keshav"
+last_name = "HL"
+
+print(first_name + " " + last_name)
+
+#Repetation/
+message = "Warning! "
+print(message * 10)
+
+#Example - 2/
+name = "keshav "
+print(name * 10)
