ad7dad4fadac3e63368c24675ed57dcb22b464d4
Keshav HL <keshavhlkesh@gmail.com>
2025-06-12 09:21:00 +0530

    Add files via upload

---
+++ python/3.prac/string methods.py
@@ -0,0 +1,17 @@
+#string Methods/
+name = "My name is keshav.  "
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.replace("keshav", "pramod"))
+
+#Example - 2/
+name = 'my name is "keshav"'
+print(name)
+
+#Example - 3/
+name = "my name is 'keshav'"
+print(name)
+
+#length of the string/
+print(len(name))
